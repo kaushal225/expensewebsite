949691c97a5679463a788bec958e19ac67fa0f42
kaushal225 <kaushal225@gmail.com>
2023-05-05 03:19:22 +0530

    deployement check

--- expensewebsite/settings.py
+++ expensewebsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hh#_5qlzly8z6c-qeg=y$5bowfx98v2!st=d0_7al+6s@qu(to'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
